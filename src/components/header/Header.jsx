37ba17595bf5dcc6baa2ce545ab1e63257e1183f
Harsh Negi <harshnegi2002@gmail.com>
2023-06-22 15:51:31 +0530

    Updated Code 23-06-2023

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,15 +3,31 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
+import Typed from 'typed.js';
 
 
 const Header = () => {
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['An Undergrad Student', '&amp; Enthusiatic Learner'],
+      typeSpeed: 50,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <header>
       <div className="container header__container">
+        
         <h5> Hello I'm </h5>
         <h1> Harsh Negi </h1>
-        <h5 className='text-light'> Student </h5>
+        <span ref={el} />
         <CTA />
         <HeaderSocials />
         <div className="me">
